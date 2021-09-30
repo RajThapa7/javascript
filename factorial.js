8273f05fefd9e49a67b61a37a53eb87ffaf7d16e
Raj Thapa <jyangoraj@gmail.com>
2021-10-01 01:46:01 +0545

    i commit
    Signed-off-by: Raj Thapa <jyangoraj@gmail.com>

---
+++ factorial.js
@@ -0,0 +1,17 @@
+// program to find the factorial of a number
+function facto(){
+    let num = document.getElementById('n').value;
+  function factorial(num){
+    
+    // if number is 0
+    if (num == 0) {
+        return 1;
+    }
+  
+    // if number is positive
+    else {
+        return  num * factorial(num - 1);
+    }
+  };
+  document.getElementById("demo").innerHTML = `Factorial is ${factorial(num)} `;
+  }
